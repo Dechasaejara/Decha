94014315a432e4ecc556969ec51003be7eedf270
D <d@Ds-MacBook-Pro.local>
2023-10-04 23:00:57 +0300

    add dynamic card btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,16 +56,18 @@ navToggler.addEventListener("click", toggleNavbar);
  * active header when window scrolled to 50px
  */
 
-const header = document.querySelector("[data-header]");
+// const header = document.querySelector("[data-header]");
 
-const activeHeader = function () {
-  window.scrollY > 50 ? header.classList.add("active ")
-    : header.classList.remove("active ");
-}
+// const activeHeader = function () {
+//   window.scrollY > 50 ? header.classList.add("active ")
+//     : header.classList.remove("active ");
+// }
 
-window.addEventListener("scroll", activeHeader);
+// window.addEventListener("scroll", activeHeader);
 
 
+
+// change header bg when scroll
 const header1 = document.querySelector('.header');
 
 function changeHeaderBackground() {
@@ -77,3 +79,51 @@ function changeHeaderBackground() {
 }
 
 window.addEventListener('scroll', changeHeaderBackground);
+
+// Show dynamic btn
+const cards = document.querySelectorAll('.portfolio-card');
+const popupContainer = document.querySelector('#popup-container');
+const closeBtn = document.getElementById('popup-close-btn');
+
+
+cards.forEach(card => {
+  const button = card.querySelector('.dynamic-btn');
+
+  card.addEventListener('mouseenter', () => {
+    button.style.visibility = 'visible';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    button.style.visibility = 'hidden';
+  });
+
+
+  button.addEventListener('click', function () {
+    centerPopup();
+    popupContainer.style.display = 'flex'; // Show the popup
+
+  });
+
+
+});
+
+// Create popup
+
+closeBtn.addEventListener('click', function () {
+  popupContainer.style.display = 'none'; // Hide the popup
+});
+
+function centerPopup() {
+  const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+  const popupWidth = popupContainer.offsetWidth;
+  const popupHeight = popupContainer.offsetHeight;
+
+  const left = (viewportWidth - popupWidth) / 2;
+  const top = (viewportHeight - popupHeight) / 2;
+
+  popupContainer.style.left = `${left}px`;
+  popupContainer.style.top = `${top}px`;
+}
+
+window.addEventListener('resize', centerPopup);
